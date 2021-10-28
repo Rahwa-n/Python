eca10c86ba208e6948016928c74597cc1af4f043
rahwa3netsereab@gmail.com <rahwa3netsereab@gmail.com>
2021-10-28 14:07:47 -0500

    adding more notes and practice

--- practice2.py
+++ practice2.py
@@ -9,5 +9,14 @@ print("Monty Python.")
 print("My name is ", end=" \n")
 print("Monty Python.")
 
-the sep is going to sepereate with hyphen
+
 print("My", "name", "is", "Monty", "Python.", sep="-")
+
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+print("Programming","Essentials","in")
+print("Python")
+
+print("Programming", "Essentials", "in", sep="***", end="...")
+print("Python")
