34664c8515e6f93e7605805284d0eb560a257656
rahwa3netsereab@gmail.com <rahwa3netsereab@gmail.com>
2022-08-03 09:58:45 -0500

    more notes and exercises

---
+++ input_function_python.py
@@ -0,0 +1,10 @@
+print("Tell me anything...")
+anything = input()
+print("Hmm...", anything, "... Really?")
+
+
+anything = input("Enter a number: ")
+something = anything ** 2.0
+print(anything, "to the power of 2 is", something)
+
+
